7d7975fc814fadaa33ec829446062dc2c5547ce2
partha360 <hellopartha@outlook.com>
2018-03-05 10:47:29 +0530

    Added Offline and Web Manifest Support

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,12 +30,50 @@ module.exports = {
     },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
-/*     {
-      resolve: 'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        plugins: [],
+        name: "finiteloop",
+        short_name: "finiteloop",
+        start_url: "/",
+        background_color: "#f7f0eb",
+        theme_color: "#424242",
+        display: "minimal-ui",
+        icons: [
+          {
+            // Everything in /static will be copied to an equivalent
+            // directory in /public during development and build, so
+            // assuming your favicons are in /static/favicons,
+            // you can reference them here
+            src: `/favicons/android-icon-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-icon-144x144.png`,
+            sizes: `144x144`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-icon-96x96.png`,
+            sizes: `96x96`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-icon-72x72.png`,
+            sizes: `72x72`,
+            type: `image/png`,
+          },
+        ],
       },
-    }, */
+    },
+    `gatsby-plugin-offline`,
+    /*     {
+          resolve: 'gatsby-transformer-remark',
+          options: {
+            plugins: [],
+          },
+        }, */
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
